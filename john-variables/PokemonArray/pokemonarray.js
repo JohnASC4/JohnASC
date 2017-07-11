88f282a1249613a42c28fe62b6184f471f240e34
JohnASC4 <merejojohn@gmail.com>
2017-07-11 13:15:27 -0400

    first commit

---
+++ john-variables/PokemonArray/pokemonarray.js
@@ -0,0 +1,14 @@
+var myPokemonRoster = ["garchomp", "manaphy","mimikyu", "zapdos", "skarmory"];
+var reversed = myPokemonRoster.reverse();
+
+var firstToFight = myPokemonRoster [0];
+var secondToFight = myPokemonRoster [1];
+
+
+//console.log(firstToFight);
+//console.log(secondToFight);
+//myPokemonRoster.pop();
+
+for (var i=0; i<myPokemonRoster.length; i=5-i){
+console.log(myPokemonRoster[i]);
+}
---
+++ john-variables/pokemonVariable/pokemon.js
@@ -0,0 +1,13 @@
+var garchompHP = 108;
+var garchompNum = 445;
+var garchompType = "Dragon and Ground";
+var garchompMove = "Outrage";
+var garchompLastEvo = true;
+
+var garchompAttack = 130;
+var garchompDefense = 95;
+var garchompSpecialAttack = 80;
+var garchompSpecialDefense = 85;
+var garchompSpeed = 102;
+
+console.log(garchompAttack);
