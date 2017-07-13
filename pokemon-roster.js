cf445155d667c73158e3316c28efb4f3df9f78c7
JohnASC4 <merejojohn@gmail.com>
2017-07-13 12:00:20 -0400

    pokemon roster arrays

---
+++ pokemon-roster.js
@@ -0,0 +1,5 @@
+var pokemons = ["manaphy","charizard","pikachu","geodude","zapdos","mew"];
+
+var manaphy = ["HP:100", "DEF:100, ATK:100, TYPE:WATER", "Legend:Yes"];
+
+var charizard = ["HP:70","DEF:65","ATK:90","TYPE:FIRE"]
