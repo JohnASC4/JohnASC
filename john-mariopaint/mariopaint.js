1796153ffc7f384f78820d93fc39d92b0d9956de
JohnASC4 <merejojohn@gmail.com>
2017-07-17 15:17:28 -0400

    new projects

---
+++ john-mariopaint/mariopaint.js
@@ -0,0 +1,13 @@
+function setup(){
+createCanvas(500,500);
+background("blue");
+}
+function mouseDragged() {
+var colors = [ "yellow", "red", "yellow", "red" ];
+var color = random(colors);  // select random word
+    fill(color);
+  ellipse(mouseX, mouseY, random([50],[80]), random([60],[100]));
+
+  // prevent default
+  return false;
+}
---
+++ john-mariopaintv2/mariopaintv2.js
@@ -0,0 +1,25 @@
+function setup(){
+createCanvas(500,500);
+background("green");
+fill("red");
+rect(30, 20, 55, 55);
+
+(fill("yellow"));
+rect(110,20,55,55);
+
+(fill("blue"));
+rect(190,20,55,55);
+
+}
+function mousePressed() {
+  ellipse(mouseX, mouseY, 5, 5);
+function mouseDragged() {
+var colors = [ "yellow", "red", "yellow", "red" ];
+var color = random(colors);  // select random word
+    fill(color);
+  ellipse(mouseX, mouseY, random([50],[80]), random([60],[100]));
+
+  // prevent default
+  return false;
+}
+
