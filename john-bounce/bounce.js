a6870019435f9dbb648851856c934b5c2f5307af
JohnASC4 <merejojohn@gmail.com>
2017-07-18 19:48:58 -0400

    p5projects

---
+++ john-bounce/bounce.js
@@ -0,0 +1,34 @@
+var x = 50; // starting coordinate
+var y = 50; // starting y coordinatr
+var vx = 5; // starting
+var vy = 20;
+var speed = 5
+function setup(){
+    createCanvas(500,500);
+}
+function draw(){
+background("red");
+fill("yellow");
+ var aye = ellipse (x,y,80,80);
+x += vx;
+//x+=speed;
+//if(x>500-30/ 2){
+  //  speed = -speed;
+//}
+//if(x < 0 +30 / 2){
+  //  speed = -speed;
+//}
+//}
+text("You hungry ",100,100);
+
+ if(x>=500-25){
+ vx = -vx;
+}else if (x <= 0+25){
+ vx = -vx;
+}
+}
+function mousePressed(){
+ if(aye){
+colorPick ="green";
+ }
+}
---
+++ john-typetest/typetest.js
@@ -0,0 +1,26 @@
+
+function setup(){
+ createCanvas(500,500);
+ background("black");
+}
+
+function draw() {
+  fill("white");
+  textSize(20);
+  textFont("Georgia");
+  text("Please type the sentence below",10,30);
+  text("Come as you are", 10, 100);
+  text("Current Points "+ points,10,400);
+    }
+var points = 0;
+  function keyTyped() {
+  if (key === 'c') {
+    points = 240;
+  } else if (text = "You need to work on your typing",10,500) {
+    points = -10
+  }
+return false;
+  }
+
+
+
